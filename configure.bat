b152d72ddbfbcdf149911dd099d7875b534b1ef2
Jose Gonzalez Lopez <joseg072103@gmail.com>
2024-01-14 08:21:58 -0500

    switched to glfw submodule instead of FetchContent

--- configure.bat
+++ configure.bat
@@ -1,3 +1,3 @@
 @echo off
 xcopy /s /e /y resources build\resources\
-cmake -S . -B build -G "MinGW Makefiles"
+cmake -S . -B build -G "MinGW Makefiles" -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
